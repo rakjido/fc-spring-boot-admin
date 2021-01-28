995944a2706b2e3b242ec89460c68a9601024019
rakjido <ndr.zenn@gmail.com>
2021-01-28 20:32:31 +0900

    [CLN] clean, refactoring

--- src/main/java/io/rooftop/admin/service/ItemApiLogicService.java
+++ src/main/java/io/rooftop/admin/service/ItemApiLogicService.java
@@ -4,6 +4,7 @@ import io.rooftop.admin.dto.ItemStatus;
 import io.rooftop.admin.entity.Item;
 import io.rooftop.admin.network.Header;
 import io.rooftop.admin.network.request.ItemApiRequestDto;
+import io.rooftop.admin.network.request.OrderGroupApiRequestDto;
 import io.rooftop.admin.network.response.ItemApiResponseDto;
 import io.rooftop.admin.repository.ItemRepository;
 import io.rooftop.admin.repository.PartnerRepository;
@@ -15,6 +16,102 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@RequiredArgsConstructor
+@Service
+public class ItemApiLogicService implements CrudInterface<ItemApiRequestDto, ItemApiResponseDto> {
+
+    private final ItemRepository itemRepository;
+
+    private final PartnerRepository partnerRepository;
+
+    @Override
+    public Header<ItemApiResponseDto> create(Header<ItemApiRequestDto> request) {
+        return Optional.ofNullable(request.getData())
+        .map(itemApiRequestDto -> {
+            Item item = Item.builder()
+                    .status(ItemStatus.WAITING)
+                    .name(itemApiRequestDto.getName())
+                    .title(itemApiRequestDto.getTitle())
+                    .content(itemApiRequestDto.getContent())
+                    .price(itemApiRequestDto.getPrice())
+                    .brandName(itemApiRequestDto.getBrandName())
+                    .registeredAt(LocalDateTime.now())
+                    .partner(partnerRepository.getOne(itemApiRequestDto.getPartnerId()))
+                    .build();
+            return item;
+        })
+        .map(item -> itemRepository.save(item))
+        .map(newItem -> response(newItem))
+        .orElseGet(()->Header.ERROR("No Data Found"));
+    }
+
+    @Override
+    public Header<ItemApiResponseDto> read(Long id) {
+        return itemRepository.findById(id)
+                .map(item -> response(item))
+                .orElseGet(() -> Header.ERROR("No Data Found"));
+    }
+
+    @Override
+    public Header<ItemApiResponseDto> update(Header<ItemApiRequestDto> request) {
+
+        return Optional.ofNullable(request.getData())
+                .map(requestDto -> {
+                    return itemRepository.findById(requestDto.getId());
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(item -> {
+                    ItemApiRequestDto itemApiRequestDto = request.getData();
+                    item
+                            .setStatus(itemApiRequestDto.getStatus())
+                            .setName(itemApiRequestDto.getName())
+                            .setTitle(itemApiRequestDto.getTitle())
+                            .setContent(itemApiRequestDto.getContent())
+                            .setPrice(itemApiRequestDto.getPrice())
+                            .setBrandName(itemApiRequestDto.getBrandName())
+                            .setRegisteredAt(LocalDateTime.now())
+                            .setPartner(partnerRepository.getOne(itemApiRequestDto.getPartnerId()))
+                    ;
+                    return item;
+                })
+                .map(item -> itemRepository.save(item))
+                .map(savedItem -> response(savedItem))
+                .orElseGet(() -> Header.ERROR("No Data Found"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        Optional<Item> optional = itemRepository.findById(id);
+        return optional.map(item -> {
+            itemRepository.delete(item);
+            return Header.OK();
+        })
+                .orElseGet(() -> Header.ERROR("No Data Found"));
+    }
+
+    private Header<ItemApiResponseDto> response(Item item) {
+        ItemApiResponseDto itemApiResponseDto = ItemApiResponseDto.builder()
+                .id(item.getId())
+                .status(item.getStatus())
+                .name(item.getName())
+                .title(item.getTitle())
+                .content(item.getContent())
+                .price(item.getPrice())
+                .brandName(item.getBrandName())
+                .registeredAt(item.getRegisteredAt())
+                .unregisteredAt(item.getUnregisteredAt())
+                .partnerId(item.getPartner().getId())
+                .build();
+
+        return Header.OK(itemApiResponseDto);
+    }
+}
+
+
+//=====================================
+// for BaseService
+//=====================================
 //@RequiredArgsConstructor
 //@Service
 //public class ItemApiLogicService extends BaseService<ItemApiRequestDto, ItemApiResponseDto, Item> {
@@ -112,84 +209,3 @@ import java.util.Optional;
 //    }
 //
 //}
-
-@RequiredArgsConstructor
-@Service
-public class ItemApiLogicService implements CrudInterface<ItemApiRequestDto, ItemApiResponseDto> {
-
-    private final ItemRepository itemRepository;
-
-    private final PartnerRepository partnerRepository;
-
-    @Override
-    public Header<ItemApiResponseDto> create(Header<ItemApiRequestDto> request) {
-        ItemApiRequestDto itemApiRequestDto = request.getData();
-        Item item = Item.builder()
-                .status(ItemStatus.WAITING)
-                .name(itemApiRequestDto.getName())
-                .title(itemApiRequestDto.getTitle())
-                .content(itemApiRequestDto.getContent())
-                .price(itemApiRequestDto.getPrice())
-                .brandName(itemApiRequestDto.getBrandName())
-                .registeredAt(LocalDateTime.now())
-                .partner(partnerRepository.getOne(itemApiRequestDto.getPartnerId()))
-                .build();
-        Item savedItem = itemRepository.save(item);
-        return response(savedItem);
-    }
-
-    @Override
-    public Header<ItemApiResponseDto> read(Long id) {
-        return itemRepository.findById(id)
-                .map(item -> response(item))
-                .orElseGet(() -> Header.ERROR("No Data Found"));
-    }
-
-    @Override
-    public Header<ItemApiResponseDto> update(Header<ItemApiRequestDto> request) {
-        ItemApiRequestDto itemApiRequestDto = request.getData();
-        Optional<Item> optional = itemRepository.findById(itemApiRequestDto.getId());
-
-        return optional.map(item -> {
-                item
-                    .setStatus(itemApiRequestDto.getStatus())
-                    .setName(itemApiRequestDto.getName())
-                    .setTitle(itemApiRequestDto.getTitle())
-                    .setContent(itemApiRequestDto.getContent())
-                    .setPrice(itemApiRequestDto.getPrice())
-                    .setBrandName(itemApiRequestDto.getBrandName())
-                    .setRegisteredAt(LocalDateTime.now())
-                    .setPartner(partnerRepository.getOne(itemApiRequestDto.getPartnerId()));
-            return item;
-        }).map(item -> itemRepository.save(item))
-        .map(savedItem -> response(savedItem))
-        .orElseGet(() -> Header.ERROR("No Data Found"));
-    }
-
-    @Override
-    public Header delete(Long id) {
-        Optional<Item> optional = itemRepository.findById(id);
-        return optional.map(item -> {
-            itemRepository.delete(item);
-            return Header.OK();
-        })
-                .orElseGet(() -> Header.ERROR("No Data Found"));
-    }
-
-    private Header<ItemApiResponseDto> response(Item item) {
-        ItemApiResponseDto itemApiResponseDto = ItemApiResponseDto.builder()
-                .id(item.getId())
-                .status(item.getStatus())
-                .name(item.getName())
-                .title(item.getTitle())
-                .content(item.getContent())
-                .price(item.getPrice())
-                .brandName(item.getBrandName())
-                .registeredAt(item.getRegisteredAt())
-                .unregisteredAt(item.getUnregisteredAt())
-                .partnerId(item.getPartner().getId())
-                .build();
-
-        return Header.OK(itemApiResponseDto);
-    }
-}
--- src/main/java/io/rooftop/admin/service/UserApiLogicService.java
+++ src/main/java/io/rooftop/admin/service/UserApiLogicService.java
@@ -1,7 +1,9 @@
 package io.rooftop.admin.service;
 
+import io.rooftop.admin.dto.ItemStatus;
 import io.rooftop.admin.dto.OrderType;
 import io.rooftop.admin.dto.UserStatus;
+import io.rooftop.admin.entity.Item;
 import io.rooftop.admin.entity.OrderGroup;
 import io.rooftop.admin.entity.User;
 import io.rooftop.admin.network.Header;
@@ -16,6 +18,102 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserApiLogicService implements CrudInterface<UserApiRequestDto, UserApiResponseDto> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Header<UserApiResponseDto> create(Header<UserApiRequestDto> requestDto) {
+
+        return Optional.ofNullable(requestDto.getData())
+                .map(userApiRequestDto -> {
+                    User user = User.builder()
+                            .account(userApiRequestDto.getAccount())
+                            .password(userApiRequestDto.getPassword())
+                            .status(UserStatus.REGISTERED)
+                            .phoneNumber(userApiRequestDto.getPhoneNumber())
+                            .email(userApiRequestDto.getEmail())
+                            .registeredAt(LocalDateTime.now())
+                            .build();
+                    return user;
+                })
+                .map(user -> userRepository.save(user))
+                .map(newUser -> response(newUser))
+                .orElseGet(()->Header.ERROR("No Data Found"));
+    }
+
+    @Override
+    public Header<UserApiResponseDto> read(Long id) {
+        return userRepository.findById(id)
+                .map(user -> response(user))
+                .orElseGet(()-> Header.ERROR("No Data found"));
+    }
+
+    @Override
+    public Header<UserApiResponseDto> update(Header<UserApiRequestDto> requestDto) {
+
+        return Optional.ofNullable(requestDto.getData())
+                .map(userApiRequestDto -> {
+                    return userRepository.findById(userApiRequestDto.getId());
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(user -> {
+                    UserApiRequestDto userApiRequestDto = requestDto.getData();
+                    user.setAccount(userApiRequestDto.getAccount())
+                            .setPassword(userApiRequestDto.getPassword())
+                            .setPhoneNumber(userApiRequestDto.getPhoneNumber())
+                            .setEmail(userApiRequestDto.getEmail())
+                            .setStatus(userApiRequestDto.getStatus())
+                            .setRegisteredAt(userApiRequestDto.getRegisteredAt())
+                            .setUnregisteredAt(userApiRequestDto.getUnregisteredAt())
+                    ;
+                    return user;
+                })
+                .map(user -> userRepository.save(user))
+                .map(updatedUser -> response(updatedUser))
+                .orElseGet(() -> Header.ERROR("No Data Found"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        Optional<User> optional = userRepository.findById(id);
+
+        return optional.map(user -> {
+            userRepository.delete(user);
+            return Header.OK();
+        })
+                .orElseGet(()->Header.ERROR("No Data Found"));
+
+    }
+
+    private Header<UserApiResponseDto> response(User user){
+
+        UserApiResponseDto userApiResponseDto = UserApiResponseDto.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // todo 암호화, 길이 변환 등
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        return Header.OK(userApiResponseDto);
+    }
+}
+
+
+//=====================================
+// for BaseService
+//=====================================
 
 //@Slf4j
 //@RequiredArgsConstructor
@@ -138,86 +236,3 @@ import java.util.Optional;
 //        return Header.OK(userApiResponseDto);
 //    }
 //}
-
-
-@Slf4j
-@RequiredArgsConstructor
-@Service
-public class UserApiLogicService implements CrudInterface<UserApiRequestDto, UserApiResponseDto> {
-
-    private final UserRepository userRepository;
-
-    @Override
-    public Header<UserApiResponseDto> create(Header<UserApiRequestDto> requestDto) {
-        UserApiRequestDto userApiRequestDto = requestDto.getData();
-        log.info(userApiRequestDto.toString());
-        User user = User.builder()
-                .account(userApiRequestDto.getAccount())
-                .password(userApiRequestDto.getPassword())
-                .status(UserStatus.REGISTERED)
-                .phoneNumber(userApiRequestDto.getPhoneNumber())
-                .email(userApiRequestDto.getEmail())
-                .registeredAt(LocalDateTime.now())
-                .build();
-
-        User newUser = userRepository.save(user);
-        return response(newUser);
-    }
-
-    @Override
-    public Header<UserApiResponseDto> read(Long id) {
-        return userRepository.findById(id)
-                .map(user -> response(user))
-                .orElseGet(()-> Header.ERROR("No Data found"));
-    }
-
-    @Override
-    public Header<UserApiResponseDto> update(Header<UserApiRequestDto> requestDto) {
-        UserApiRequestDto userApiRequestDto = requestDto.getData();
-        Optional<User> optional = userRepository.findById(userApiRequestDto.getId());
-
-        return optional.map(user -> {
-            user.setAccount(userApiRequestDto.getAccount())
-                    .setPassword(userApiRequestDto.getPassword())
-                    .setPhoneNumber(userApiRequestDto.getPhoneNumber())
-                    .setEmail(userApiRequestDto.getEmail())
-                    .setStatus(userApiRequestDto.getStatus())
-                    .setRegisteredAt(userApiRequestDto.getRegisteredAt())
-                    .setUnregisteredAt(userApiRequestDto.getUnregisteredAt())
-            ;
-            return user;
-        })
-                .map(user -> userRepository.save(user))
-                .map(updatedUser -> response(updatedUser))
-                .orElseGet(()->Header.ERROR("No Data Found"));
-
-    }
-
-    @Override
-    public Header delete(Long id) {
-        Optional<User> optional = userRepository.findById(id);
-
-        return optional.map(user -> {
-            userRepository.delete(user);
-            return Header.OK();
-        })
-                .orElseGet(()->Header.ERROR("No Data Found"));
-
-    }
-
-    private Header<UserApiResponseDto> response(User user){
-
-        UserApiResponseDto userApiResponseDto = UserApiResponseDto.builder()
-                .id(user.getId())
-                .account(user.getAccount())
-                .password(user.getPassword()) // todo 암호화, 길이 변환 등
-                .email(user.getEmail())
-                .phoneNumber(user.getPhoneNumber())
-                .status(user.getStatus())
-                .registeredAt(user.getRegisteredAt())
-                .unregisteredAt(user.getUnregisteredAt())
-                .build();
-
-        return Header.OK(userApiResponseDto);
-    }
-}
